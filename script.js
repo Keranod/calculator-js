b4bc92c2f77c4e47a9965bab9bec4ea768439caa
Konrad Konkel <konrad.konkel@wp.pl>
2024-05-08 09:12:04 +0100

    multiplyAction and divideAction added

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const isNumber = (value) => {
 }
 
 const sumAction = (value1, value2) => {
-    if (!isNumber(value1) || !isNumber(value2)) {
-        return "Error";
+    if (!isNumber(value1)) {
+        return "Error, value1 is not a number";
+    }
+    if (!isNumber(value2)) {
+        return "Error, value2 is not a number";
     }
     return value1 + value2;
 }
@@ -17,8 +20,11 @@ console.log(sumAction(null,45))
 console.log(sumAction(12,"ASD"))
 
 const substractAction = (value1, value2) => {
-    if (!isNumber(value1) || !isNumber(value2)) {
-        return "Error";
+    if (!isNumber(value1)) {
+        return "Error, value1 is not a number";
+    }
+    if (!isNumber(value2)) {
+        return "Error, value2 is not a number";
     }
     return value1 - value2;
 }
@@ -29,3 +35,40 @@ console.log(substractAction(12.44,422))
 console.log(substractAction(-112,0.45))
 console.log(substractAction(null,45))
 console.log(substractAction(12,"ASD"))
+
+const multiplyAction = (value1, value2) => {
+    if (!isNumber(value1)) {
+        return "Error, value1 is not a number";
+    }
+    if (!isNumber(value2)) {
+        return "Error, value2 is not a number";
+    }
+    return value1 * value2;
+}
+
+console.log("multiplyAction")
+console.log(multiplyAction(12,45))
+console.log(multiplyAction(12.44,422))
+console.log(multiplyAction(-112,0.45))
+console.log(multiplyAction(null,45))
+console.log(multiplyAction(12,"ASD"))
+
+const divideAction = (value1, value2) => {
+    if (!isNumber(value1)) {
+        return "Error, value1 is not a number";
+    }
+    if (!isNumber(value2)) {
+        return "Error, value2 is not a number";
+    }
+    if (value2 === 0) {
+        return "Error, cannot divide by '0'"
+    }
+    return value1 / value2;
+}
+
+console.log("divideAction")
+console.log(divideAction(12,45))
+console.log(divideAction(12.44,0))
+console.log(divideAction(-112,0.45))
+console.log(divideAction(null,45))
+console.log(divideAction(12,"ASD"))
